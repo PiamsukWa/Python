156dbdb4b9ed161ef1ebef9bec11c3a264d9581a
Piamsuk Wathiroiram <Piamsuk.Wathiroiram@kkumail.com>
2021-02-11 19:49:14 +0700

    update

--- Week5/Week5.1.py
+++ Week5/Week5.1.py
@@ -1,24 +1,27 @@
 #แบบฝึกหัด 5.1
 def add():
-    global name,grade,sex,province,department
-    print("-"*20+"แนะนำตัว"+"-"*20)
-    add = input("ชื่อ-สกุล  :  ชั้น  :  เพศ  : จังหวัด : สาขา         ")  
+    global name,grade,sex,province,department,faculty
+    print(""*20+"แนะนำตัว"+""*20)
+
+    add = input("ชื่อ-สกุล :  ชั้น  :  เพศ  :  จังหวัด  :  สาขา  :  คณะ         ")  
     addnew = add.split(":")
-    name = add[0]
-    grade = add[1]
-    sex = add[2]
-    province = add[3]
-    department = add[4]
+    name = addnew[0]
+    grade = addnew[1]
+    sex = addnew[2]
+    province = addnew[3]
+    department = addnew[4]
+    faculty = addnew[5]
 class nisit :
-    def __init__(self,name,grade,sex,province,department) :
+    def __init__(self,name,grade,sex,province,department,faculty) :
         self.name = name
         self.grade = grade
         self.sex = sex
         self.province = province 
         self.department = department
-    
+        self.faculty = faculty
+
     def showname(self) :
-        print("สวัสดีค่ะ ฉันชื่อ"+self.name+"เรียนอยู่ชั้นปีที่" +self.grade+ "เพศ" +self.sex+ "มาจากจังหวัด" +self.province+ "เรียนอยู่สาขา" +self.department)
+        print("สวัสดีค่ะ ฉันชื่อ    "+self.name+  "     เรียนอยู่ชั้นปีที่    "  +self.grade+  "     เพศ     "   +self.sex+  "    มาจากจังหวัด    "   +self.province+  "     เรียนอยู่สาขา   "   +self.department+   "     เรียนอยู่คณะ       "  +self.faculty)
 add()
-X = nisit(name,grade,sex,province,department)
-X.showname() 
+X = nisit(name,grade,sex,province,department,faculty)
+X.showname()
